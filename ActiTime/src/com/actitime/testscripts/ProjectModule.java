28c5185822ffcbb4df3d7b5e126874bb9000f54f
Shwetha <Shwetha@DESKTOP-EM8SP10>
2021-01-12 11:48:27 +0530

    create projtaskect

---
+++ ActiTime/src/com/actitime/testscripts/ProjectModule.java
@@ -0,0 +1,11 @@
+package com.actitime.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ProjectModule {
+@Test
+public void testCreateProject() {
+	Reporter.log("testCreateProject",true);
+}
+}
---
+++ ActiTime/src/com/actitime/testscripts/TaskModule.java
@@ -0,0 +1,11 @@
+package com.actitime.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TaskModule {
+	@Test
+	public void testCreateTask() {
+		Reporter.log("testCreateTask",true);
+	}
+}
